27dc37f9ced3d02968f7be65675fcef5e49503d4
NextFire <git@yuru.moe>
2023-01-02 00:55:31 +0100

    borg cron update

--- borg/etc/cron.sh
+++ borg/etc/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 borgmatic --verbosity 1 --list --stats
-apk add --no-cache rclone   # I'm lazy
+apk add rclone   # I'm lazy
 rclone sync /mnt/borg-repository $BORG_REPO_REMOTE -P --transfers 25 --checkers 50
