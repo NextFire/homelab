dca2497849a56c0d0debcc65ae69487c98a7b261
NextFire <git@yuru.moe>
2026-02-25 12:08:16 +0100

    borg: compact

--- system/borg/etc/cron.sh
+++ system/borg/etc/cron.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe
 borgmatic --verbosity 1 --list --stats
+borgmatic compact --progress
 apk add rclone   # I'm lazy
 rclone sync /mnt/borg-repository $BORG_REPO_REMOTE -P --transfers 25 --checkers 50
