a185e27bb1f7049d1863d8558e564bde821f1009
NextFire <git@yuru.moe>
2026-05-31 18:14:43 +0200

    update ansible things

--- ansible/roles/camp/files/fish/config.fish
+++ ansible/roles/camp/files/fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_config theme choose default-rgb
 end
-
-abbr -a pg 'docker run --pull always --rm -it -v $PWD:/projo/(basename $PWD) --hostname (hostname) nextfire/projo-generate (basename $PWD)'
-abbr -a sachi 'docker run --pull always --rm -it -v $HOME/.config/sachi:/root/.config/sachi -v $PWD:/media ghcr.io/nextfire/sachi:main rename /media'
